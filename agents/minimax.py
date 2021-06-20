dc52a4cbd9dc83514318de9ffb0d7628904db50a
Magolor <magolor@qq.com>
2021-06-20 20:38:17 +0800

    play

--- agents/minimax.py
+++ agents/minimax.py
@@ -3,4 +3,4 @@ from policy.omniscienceSearch import AlphaBetaSearch
 
 class MinimaxAgent(BaseAgent):
     def get_action(self, obs, **info):
-        return AlphaBetaSearch.sampledMinmaxAction(obs.board, self.agent_id, depth=2, truncated=True)
+        return AlphaBetaSearch.sampledMinmaxAction(obs.board, self.agent_id, depth=1, truncated=True)
--- arena.py
+++ arena.py
@@ -12,7 +12,9 @@ def play(agents, name="Default", auto_replay_id=True):
     game = env.env("generals",name=name, auto_replay_id=auto_replay_id); S, r, d, i = game.reset(), 0, False, None
     for agent_id, agent in enumerate(agents):
         agent.reset(agent_id=agent_id,obs=get_agent_batch(S,agent_id))
+    c = 0
     while not d:
+        print(c); c += 1
         actions = [agent.get_action(obs=get_agent_batch(S,agent_id),info=i) for agent_id, agent in enumerate(agents)]
         S, r, d, i = game.step(actions)
 
@@ -21,4 +23,4 @@ def play(agents, name="Default", auto_replay_id=True):
             print("Player %d wins!"%(agent_id+1))
 
 if __name__=="__main__":
-    play([agents.RandomGreedyAgent(),agents.HumanAgent(cheat=True)], name="PlayerFrameworkTesting")
+    play([agents.MinimaxAgent(),agents.MinimaxAgent()], name="PlayerFrameworkTesting")
